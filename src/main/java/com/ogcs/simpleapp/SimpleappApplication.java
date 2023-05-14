c52ab802a056265e75c45d7061837b4cd24d4fa9
Marko Pavlović <markopavlovic316@gmail.com>
2023-05-14 14:19:46 +0200

    Simple Hello world app

---
+++ src/main/java/com/ogcs/simpleapp/SimpleappApplication.java
@@ -0,0 +1,15 @@
+package com.ogcs.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication.class, args);
+		new HelloWorld();
+		System.out.println("hello");
+	}
+
+}
